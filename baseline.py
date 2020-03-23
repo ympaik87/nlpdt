d16ac4c404d814952e5c8dda31825ced7c4257ed
Young Min Paik <ympaik@shinsegae.com>
2020-03-23 17:05:59 +0900

    add some

--- baseline.py
+++ baseline.py
@@ -4,6 +4,10 @@ import re
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding, LSTM, Dense, SpatialDropout1D, Dropout
+from keras.initializers import Constant
+from keras.optimizers import Adam
 from tqdm import tqdm
 import pandas as pd
 from nltk.tokenize import word_tokenize
@@ -53,15 +57,14 @@ class TwNlp:
         return corpus
 
     def read_glove(self):
-        embedding_dict = {}
         p = self.data_p.parent/'glove.twitter.27B/glove.twitter.27B.100d.txt'
+        embedding_dict = dict()
         with open(p, 'r') as f:
             for line in f:
                 values = line.split()
                 word = values[0]
                 vectors = np.asarray(values[1:], 'float32')
                 embedding_dict[word] = vectors
-        f.close()
         return embedding_dict
 
     def prep_data(self):
@@ -69,7 +72,7 @@ class TwNlp:
         df['text'] = df['text'].swifter.apply(self.clean_text)
         return self.create_corpus_new(df['text'])
 
-    def tokenize(self):
+    def glove(self):
         max_len = 50
         tokenizer_obj = Tokenizer()
         corpus = self.prep_data()
@@ -77,7 +80,37 @@ class TwNlp:
         sequences = tokenizer_obj.texts_to_sequences(corpus)
 
         tweet_pad = pad_sequences(sequences, maxlen=max_len,
-                                truncating='post', padding='post')
+                                  truncating='post', padding='post')
+
+        word_ind = tokenizer_obj.word_index
+        num_words = len(word_ind) + 1
+        embedding_matrix = np.zeros((num_words, 100))
+
+        for word, i in tqdm(word_ind.items(), total=num_words-1,
+                            desc='emb_vec'):
+            if i < num_words:
+                emb_vec = embedding_matrix.get(word)
+                if emb_vec is not None:
+                    embedding_matrix[i] = emb_vec
+        return embedding_matrix, num_words
+
+    def model(self):
+        embedding_matrix, num_wrods = self.glove()
+        model = Sequential()
+        embedding = Embedding(
+            num_words, 100, embeddings_initializer=Constant(embedding_matrix),
+            input_length=MAX_LEN, trainable=False
+        )
+        model.add(embedding)
+        model.add(SpatialDropout1D(0.2))
+        model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+        model.add(Dense(1, activation='sigmoid'))
+
+        optimzer = Adam(learning_rate=3e-4)
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer=optimzer, metrics=['accuracy'])
+
 
 
 if __name__ == "__main__":
